6847feb7129f6462cebb55c1eb09fe3a2eafa24c
Kayne <aron.udvardi@gmail.com>
2026-05-30 18:10:10 +0200

    Added Fishing Rod category

--- src/main/java/net/kayn/apothic_fishing/ApothicFishing.java
+++ src/main/java/net/kayn/apothic_fishing/ApothicFishing.java
@@ -1,18 +1,34 @@
 package net.kayn.apothic_fishing;
 
+import net.kayn.apothic_fishing.crate.CrateRegistry;
+import net.kayn.apothic_fishing.registry.AFCreativeTabs;
+import net.kayn.apothic_fishing.registry.AFItems;
+import net.kayn.apothic_fishing.registry.AFLootCategories;
+import net.kayn.apothic_fishing.registry.AFModifiers;
+import net.kayn.fallen_gems_affixes.loot.StaffLootCategory;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 @Mod(ApothicFishing.MOD_ID)
 public class ApothicFishing {
 
     public static final String MOD_ID = "apothic_fishing";
+    public static final Logger LOGGER = LogManager.getLogger(MOD_ID);
 
     public ApothicFishing() {
         IEventBus modBus = FMLJavaModLoadingContext.get().getModEventBus();
         modBus.addListener(this::commonSetup);
+
+        AFItems.ITEMS.register(modBus);
+        AFModifiers.LOOT_MODIFIERS.register(modBus);
+
+        CrateRegistry.INSTANCE.registerToBus();
+        AFCreativeTabs.register(modBus);
+       AFLootCategories.FISHING_ROD.toString();
     }
 
     private void commonSetup(final FMLCommonSetupEvent event) {
---
+++ src/main/java/net/kayn/apothic_fishing/registry/AFModifiers.java
@@ -0,0 +1,18 @@
+package net.kayn.apothic_fishing.registry;
+
+import com.mojang.serialization.Codec;
+import net.kayn.apothic_fishing.ApothicFishing;
+import net.kayn.apothic_fishing.loot.CrateFishingModifier;
+import net.minecraftforge.common.loot.IGlobalLootModifier;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+
+public class AFModifiers {
+
+    public static final DeferredRegister<Codec<? extends IGlobalLootModifier>> LOOT_MODIFIERS =
+            DeferredRegister.create(ForgeRegistries.Keys.GLOBAL_LOOT_MODIFIER_SERIALIZERS, ApothicFishing.MOD_ID);
+
+    public static final RegistryObject<Codec<? extends IGlobalLootModifier>> CRATE_FISHING =
+            LOOT_MODIFIERS.register("crate_fishing", CrateFishingModifier.CODEC);
+}
